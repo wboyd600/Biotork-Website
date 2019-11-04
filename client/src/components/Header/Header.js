862e739bc7594b16787a4a116c8a23638d5d43e9
courtney23 <courtcross98@gmail.com>
2019-11-04 17:58:02 -0500

    Started changing header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../../assets/biotork-logo.png';
 
 const Header = () => {
     return (
         <div className='topnav'>
             {/* Logo */}
             <Link id="logo-link" to="/">
-                <img className="topnav-logo" src={ "/logo192.png" } alt="React logo" />
+                <img className="topnav-logo" src={ logo } alt="Biotork logo" />
             </Link>
 
             {/* Page Links */}
             <div className="topnav-right">
-                <Link className="topnav-link" to='/Register'>Sign in</Link>
+                <Link className="topnav-link" to='/about'>About Us</Link>
+                <Link className="topnav-link" to='/technology'>Technology</Link>
                 <Link className="topnav-link" to='/projects'>Projects</Link>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-                    Events
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
-                </a>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-                    Resources
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
-                </a>
-                <Link className="topnav-link" to="/about">About</Link>
+                <Link className="topnav-link" to='/media'>Media</Link>
+                <Link className="topnav-link" to="/contact">Contact Us</Link>
             </div>
         </div>
     )
