2f3e55bb3fe291de7ff91955d7fef1de8f6187ee
Jared Holley <jaredrholley@ufl.edu>
2019-11-04 20:42:47 -0500

    Added new page and routing functionality to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
+import Executives from "./views/Executives/Executives"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
+        <Route exact path="/Executives" component = {Executives} />
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
---
+++ client/src/views/Executives/Executives.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Executives.css';
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    This is a test of creating a new page
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
