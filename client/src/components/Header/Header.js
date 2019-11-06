b595c95a2f129937184b19987a1471314216722f
courtney23 <courtcross98@gmail.com>
2019-11-05 23:19:40 -0500

    Started using Bootstrap to style navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,25 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/biotork-logo.png';
+import {Nav, Navbar } from 'react-bootstrap';
 
-const Header = () => {
-    return (
-        <div className='topnav'>
-            {/* Logo */}
-            <Link id="logo-link" to="/">
-                <img className="topnav-logo" src={ logo } alt="Biotork logo" />
-            </Link>
 
+
+const Header = () => (
+        <Navbar expand="lg">
+        <div className='topnav'> 
+            <Navbar.Brand id="logo-link" href="/">
+                <img className="topnav-logo" src={ logo } alt="BioTork logo" />
+            </Navbar.Brand> 
+            <Nav className = 'mr-auto'>
             {/* Page Links */}
             <div className="topnav-right">
-                <Link className="topnav-link" to='/about'>About Us</Link>
-                <Link className="topnav-link" to='/technology'>Technology</Link>
-                <Link className="topnav-link" to='/projects'>Projects</Link>
-                <Link className="topnav-link" to='/media'>Media</Link>
-                <Link className="topnav-link" to="/contact">Contact Us</Link>
+                <Nav.Link href='/about'>About Us</Nav.Link>
+                <Nav.Link href='/technology'>Technology</Nav.Link>
+                <Nav.Link href='/projects'>Projects</Nav.Link>
+                <Nav.Link href='/media'>Media</Nav.Link>
+                <Nav.Link href='/contact'>Contact Us</Nav.Link>
+
             </div>
+            </Nav>
         </div>
+        </Navbar>
+        
     )
-}
+
+    
+
 
 export default Header;
