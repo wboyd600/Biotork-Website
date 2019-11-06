d07118e31ce3f837c38fb0720cf7e7c727054585
Jared Holley <jaredrholley@ufl.edu>
2019-11-06 12:31:11 -0500

    Added basic styling and changed font

--- client/src/views/Executives/Executives.js
+++ client/src/views/Executives/Executives.js
@@ -4,14 +4,22 @@ import './Executives.css';
 
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    This is a test of creating a new page
-                </p>
-            </header>
+        <div class="container">
+            <div class="row">
+                <div class="heading-title text-center">
+                    <h3> Our professionals </h3>
+                    <p class="center-txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
         </div>
+        
+
+
+
     );
 }
+
+
+
 
 export default Home;
