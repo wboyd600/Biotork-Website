8f3a8f3954474394f084373b5c6c4de9eda0e8e4
courtney23 <courtcross98@gmail.com>
2019-11-06 20:29:09 -0500

    Finished styling of navbar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,23 +7,22 @@ import {Nav, Navbar } from 'react-bootstrap';
 
 
 const Header = () => (
-        <Navbar expand="lg">
-        <div className='topnav'> 
+        <Navbar bg="light" variant="light">
+       
             <Navbar.Brand id="logo-link" href="/">
                 <img className="topnav-logo" src={ logo } alt="BioTork logo" />
             </Navbar.Brand> 
             <Nav className = 'mr-auto'>
             {/* Page Links */}
-            <div className="topnav-right">
+
                 <Nav.Link href='/about'>About Us</Nav.Link>
                 <Nav.Link href='/technology'>Technology</Nav.Link>
                 <Nav.Link href='/projects'>Projects</Nav.Link>
                 <Nav.Link href='/media'>Media</Nav.Link>
                 <Nav.Link href='/contact'>Contact Us</Nav.Link>
 
-            </div>
+
             </Nav>
-        </div>
         </Navbar>
         
     )
