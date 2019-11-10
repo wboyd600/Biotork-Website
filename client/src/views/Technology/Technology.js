517b7cf299c398fb1c8404076955640accd592f7
wboyd600 <wboyd600@ufl.edu>
2019-11-10 15:31:18 -0500

    added technology page

---
+++ client/src/views/Technology/Technology.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Technology() {
+    return (
+        <div className="App">
+            <header className="App-header">
+               
+            </header>
+        </div>
+    );
+}
+
+export default Technology;
