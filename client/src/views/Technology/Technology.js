4e9d72d8c0d3610fa798b02745a44c5c4f3f2bfa
Dylan Brisco <dylanbrisco@ufl.edu>
2019-11-10 17:36:36 -0500

    testing adding background images to cards

--- client/src/views/Technology/Technology.js
+++ client/src/views/Technology/Technology.js
@@ -8,8 +8,8 @@ function Technology() {
         </head>
         <div class="container">
             <div class="row">
-            <div class="hero-section">
-            <div class="hero-section-text">
+            <div class="hero-section" style={{  background: "url(" + "https://alzheimersnewstoday.com/wp-content/uploads/2016/03/shutterstock_114563296-1400x480@2x.jpg" + ")"}}>
+            <div class="hero-section-text"> 
             <div class="heading-title text-center">
                     <h1> Our Technologies </h1>
                 </div>
@@ -37,12 +37,13 @@ function Technology() {
             <div class="flex-container">
             <div class="column">
                 <div class="card">
-                <img class="card-img-top" src="https://image.freepik.com/free-photo/portrait-middle-aged-businessman-posing-looking-camera_1163-4800.jpg" alt="Jane"></img>
-                <div class="container">
+                <div class="hero-section" style={{  background: "url(" + "https://media.licdn.com/dms/image/C561BAQG1SIL0uFUQ1A/company-background_10000/0?e=2159024400&v=beta&t=vD9AZT70vkE9pMDv7NQJ7LVl3o2qaly8CbGRS726_T4" + ")"}}>
+                <div class="container" >
                     <h2>James Bond</h2>
                     <p class="title">CEO &amp; Founder</p>
                     <p>lorem ipsum ipsum lorem.</p>
                     <p>CEO@test.com</p>
+                    </div>
                 </div>
                 </div>
             </div>
