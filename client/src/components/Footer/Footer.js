91802e8229b9301f9dffc7903fa7b2247b150476
Jared Holley <jaredrholley@ufl.edu>
2019-11-10 12:28:15 -0500

    Added FA text and CDN

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
+        <>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
         <div class="footer">
             <div class="footer-content">
-                <div class="footer-section about"></div>
+                <div class="footer-section about">
+                    <h1><span>BioTork</span></h1>
+                    <p>
+                        Here at BioTork, our mission is to improve world 
+                        sustainability and the environmental conditions 
+                        of our planet for generations to come.
+                    </p>
+                    <div class="contact">
+                        <span><i class="fa fa-phone"></i> &nbsp;123-456-789</span><br></br>
+                        <span><i class="fa fa-envelope"></i> &nbsp;test@BioTork.com</span>
+                    </div>
+                    <div class="socials">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                    </div>
+                </div>
                 <div class="footer-section links"></div>
                 <div class="footer-section contact-form"></div>
             </div>
@@ -13,6 +33,7 @@ const Footer = () => {
             </div>
 
         </div>
+        </>
     );
 }
 
