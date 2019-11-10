c91cc46fcb475709655568d486d29cf92e58f0cd
Jared Holley <jaredrholley@ufl.edu>
2019-11-09 20:03:29 -0500

    Basic styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,11 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div class="footer">
-            <div class="footer-content"></div>
+            <div class="footer-content">
+                <div class="footer-section about"></div>
+                <div class="footer-section links"></div>
+                <div class="footer-section contact-form"></div>
+            </div>
             <div class="footer-bottom">
                 &copy; BioTork 2019 | Designed By Group 5B
             </div>
