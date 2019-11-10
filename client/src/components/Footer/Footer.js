dbbdc925480ad15920cc768f2d72e9751991b9fd
Jared Holley <jaredrholley@ufl.edu>
2019-11-09 19:19:23 -0500

    Added basic footer structure

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+    return (
+        <div className = "App">
+            MY FOOTER
+        </div>
+    );
+}
+
+export default Footer;
