7ce3c1c4131afb2dd0a561b0f340369b41eee540
Jared Holley <jaredrholley@ufl.edu>
2019-11-09 19:35:37 -0500

    Implemented Sticky/Fixed Footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         </Route>
         <Route component={NotFound}/>
       </Switch>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div className = "App">
-            MY FOOTER
+        <div class="footer">
+            <p>Posted by: Hege Refsnes</p>
+            <p>Contact information: <a href="mailto:someone@example.com">
+            someone@example.com</a>.</p>
         </div>
     );
 }
