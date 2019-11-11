707d1edb0a7b9f75817d68caf1fc43e3967bd4c8
Dylan Brisco <dylanbrisco@ufl.edu>
2019-11-10 22:11:06 -0500

    Added text to cards

--- client/src/views/Technology/Technology.js
+++ client/src/views/Technology/Technology.js
@@ -47,10 +47,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://www.darpa.mil/DDM_Gallery/BTOOfficewide619-316.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>GREATER PRODUCT YIELDS</h2>
                             </div>
                         </div>
                     </div>
@@ -60,10 +57,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://news.crunchbase.com/wp-content/uploads/2018/05/molecular-explosion-abstract-science-background-illustration-id496698386-1.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>HIGHER GROWTH RATES</h2>
                             </div>
                         </div>
                     </div>
@@ -74,10 +68,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://dr282zn36sxxg.cloudfront.net/datastreams/f-d%3Adfddd2a117538bc697836b2359cdd805860b7cdd5a9f194afdad6ec8%2BIMAGE_TINY%2BIMAGE_TINY.1" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>HIGHER PRODUCTIVITY</h2>
                             </div>
                         </div>
                     </div>
@@ -87,10 +78,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "http://www.indiaeducation.net/imagesvr_ce/5731/bg-aperfeicoamento.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>HIGHER TITERS</h2>
                             </div>
                         </div>
                     </div>
@@ -101,10 +89,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://uwaterloo.ca/future-students/sites/ca.future-students/files/uploads/images/contract-628-171208-science-stock-6610.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>HIGHER CULTURE DENSITIES</h2>
                             </div>
                         </div>
                     </div>
@@ -114,10 +99,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://crosstalk.cell.com/hs-fs/hubfs/Images/Matt%20Pavlovich/Talking%20about%20biotechnology/sequencing%20featured.jpg?width=769&name=sequencing%20featured.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>HIGHER TOLERANCE TO PRODUCT INHIBITION OR TO TOXIC COMPOUNDS</h2>
                             </div>
                         </div>
                     </div>
@@ -128,10 +110,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://www.wur.nl/upload_mm/2/a/3/47584885-db94-4d8f-af9c-3e401b41260e_2173890_blauwe%20cellen_B_2add4f09_530x397.jpg" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>ROBUST GROWTH AT NON-OPTIMAL TEMPERATURES, pH OR AERATION RATES</h2>
                             </div>
                         </div>
                     </div>
@@ -141,10 +120,7 @@ function Technology() {
                     <div class="card">
                         <div class="hero-section" style={{  background: "url(" + "https://media.licdn.com/dms/image/C561BAQG1SIL0uFUQ1A/company-background_10000/0?e=2159024400&v=beta&t=vD9AZT70vkE9pMDv7NQJ7LVl3o2qaly8CbGRS726_T4" + ")"}}>
                             <div class="container" >
-                                <h2>James Bond</h2>
-                                <p class="title">CEO &amp; Founder</p>
-                                <p>lorem ipsum ipsum lorem.</p>
-                                <p>CEO@test.com</p>
+                                <h2>UTILIZATION OF NON-OPTIMAL CARBON</h2>
                             </div>
                         </div>
                     </div>
