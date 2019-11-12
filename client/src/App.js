5f3c2bb932d18add92627810533421863b4707a6
carlycuadra <carlycuadra@ufl.edu>
2019-11-11 20:17:54 -0500

    updated timeline component- finsihed

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
 
 
+import Timeline from "./components/Timeline/Timeline"
 const App = () => {
   return (
     <div>
-      <Header />
+     
       <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
+        <Route exact path="/Timeline" component={Timeline} />
         <Route component={NotFound}/>
       </Switch>
     </div>
