b376c6033104d2bd80916e0ee64778c659ff2d21
wboyd600 <wboyd600@ufl.edu>
2019-11-11 22:05:22 -0500

    trying to fix merge changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
+import Home from "./views/Home/Home"
+import Executives from "./views/Executives/Executives"
 
 import NotFound from "./views/NotFound"
+import Header from "./components/Header/Header"
+import Technology from "./views/Technology/Technology"
 import Timeline from "./components/Timeline/Timeline"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
     <div>
-     
+      <Header />
+
       <Switch>
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Executives" component = {Executives} />
+        <Route exact path="/">
+          <Redirect to="/Home" />
+        </Route>
+        <Route exact path="/Technology" component={Technology} />
         <Route exact path="/Timeline" component={Timeline} />
-        
-        
+
+
         <Route component={NotFound}/>
       </Switch>
+      <Footer />
     </div>
   );
 }
-
 export default App;
