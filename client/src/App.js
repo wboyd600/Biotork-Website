c4d09ae2212a710a2557d1c59be9a8d91a422aca
carlycuadra <32986263+carlycuadra@users.noreply.github.com>
2019-11-11 20:55:16 -0500

    final app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import Executives from "./views/Executives/Executives"
+
 import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from "./components/Footer/Footer"
+import Timeline from "./components/Timeline/Timeline"
+
 
 const App = () => {
   return (
     <div>
-      <Header />
+     
       <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/Executives" component = {Executives} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
+        <Route exact path="/Timeline" component={Timeline} />
+        
+        
         <Route component={NotFound}/>
       </Switch>
-      <Footer />
     </div>
   );
 }
