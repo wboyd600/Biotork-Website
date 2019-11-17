a75bdd41918302f07431434101e83670747497a4
courtney23 <courtcross98@gmail.com>
2019-11-17 13:54:01 -0500

    made navbar mobile responsive

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,21 +8,28 @@ import {Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => (
 
-        <Navbar bg="light" variant="light" fixed="top">
+        <Navbar bg="light" variant="light" fixed="top" expand="sm">
        
             <Navbar.Brand id="logo-link" href="/">
                 <img className="topnav-logo" src={ logo } alt="BioTork logo" />
             </Navbar.Brand> 
-            <Nav>
-
+            
             {/* Page Links */}
-               
+        {/*  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navLinks" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button> */}
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav classname="ml-auto">
                 <Nav.Link href='/about'>About Us</Nav.Link>
                 <Nav.Link href='/technology'>Technology</Nav.Link>
                 <Nav.Link href='/projects'>Projects</Nav.Link>
                 <Nav.Link href='/media'>Media</Nav.Link>
                 <Nav.Link href='/contact'>Contact Us</Nav.Link> 
+        
                 </Nav>
+                </Navbar.Collapse>
         </Navbar>
 
         
