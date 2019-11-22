c89378720986b032a78f1fec10746ae67e642219
carlycuadra <carlycuadra@ufl.edu>
2019-11-22 14:09:32 -0500

    took out timeline directory

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                 <div class="footer-section about">
                     <h1><span>BioTork</span></h1>
                     <p>
-                        Here at BioTork, our mission is to improve world 
+                        Here at BioTork, our mission i4s to improve world 
                         sustainability and the environmental conditions 
                         of our planet for generations to come.
                     </p>
@@ -19,10 +19,10 @@ const Footer = () => {
                         <span><i class="fa fa-envelope"></i> &nbsp;test@BioTork.com</span>
                     </div>
                     <div class="socials">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-youtube"></i></a>
+                        <a href=""><i class="fa fa-facebook"></i></a>
+                        <a href=""><i class="fa fa-instagram"></i></a>
+                        <a href=""><i class="fa fa-twitter"></i></a>
+                        <a href=""><i class="fa fa-youtube"></i></a>
                     </div>
                 </div>
                 <div class="footer-section links">
