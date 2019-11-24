092915f28872be6d521c57eb7c65f81fcffc2d73
courtney23 <courtcross98@gmail.com>
2019-11-23 20:07:20 -0500

    changed out dummy text for mission

--- client/src/views/AboutUs/AboutUs.js
+++ client/src/views/AboutUs/AboutUs.js
@@ -29,7 +29,19 @@ function AboutUs() {
             </div>
 
             <div>
-                <p>Founded in 2008 and based in Gainesville, FL, BioTork has become a world leader in the evolutionary optimization of fermentation processes. Through access to a unique proprietary technology, we use natural selection to improve the microorganisms that carry out fermentation.</p>
+                <p>Here at BioTork, our mission is to improve world sustainability and the environmental conditions of our planet for generations to come.</p>
+            </div>
+
+            <div>
+                <p>Recycling waste, sustainable agriculture and renewable energy are threads that run through all of the projects we undertake. By improving biological processes, we convert low-value agricultural by-products to commodities such as renewable energy and high value supplements.</p>
+            </div>
+
+            <div>
+                <p>Most importantly, we carry out our improvement of these biological process through evolution rather than genetic engineering. This allows us to create optimum NON-GMO cellular factories for the conversion of feedstock into desirable commodities. However, it also allows us to improve already genetically modified organisms for traits such as robustness if desired.</p>
+            </div>
+
+            <div>
+                <p>As a company, we are dedicated to preserving the gifts and harnessing the power of nature.</p>
             </div>
         </div>
 
