b08ae1922be28f358ecd72143627678e445fd7e6
Dylan Brisco <dylanbrisco@ufl.edu>
2019-11-24 21:53:56 -0500

    Robust page finished

--- client/src/views/Technology/Robust.js
+++ client/src/views/Technology/Robust.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Technology.css';
 
-function ProductYield() {
+function Robust () {
     return (
         <>
         <div class="container">
             <div class="row">
-                <div class="hero-section" style={{  background: './Product_Yields_Background.jpg'}}>
+                <div class="robust-hero-section" style={{  background: './Robust.jpg'}}>
                     <div class="hero-section-text"> 
                         <div class="heading-title text-center">
-                            <h1> Greater Product Yield </h1>
+                            <h1> Robust Growth At Non-Optimal Temperatures, pH Or Aeration Rates That Are Often Encountered In Real-World Fermentations </h1>
                         </div>
                     </div>
                 </div>
@@ -19,7 +19,7 @@ function ProductYield() {
         <div class="container">
             <div class="row">
                 <div class="heading-detail text-center">
-                    <p class="center-txt ">Product yield, also known as stoichiometric yield, is a measure of the efficiency of conversion of feedstock to product. A 50% conversion means that for every metric ton of feedstock used, 500 kg of product are produced. For every product and feedstock there is a theoretical maximum yield. For example, for the conversion of sugar to ethanol the theoretical maximum yield is 51%, however, most industrial ethanol producers get a yield of 45-48%. This is due to inefficiencies in the process and the production of off-target products like biomass and other metabolites. The increase in flux to the desired end product will increase product yield and, subsequently, titer and productivity. Evolutionary optimization is an excellent way to improve stoichiometric yield, especially when product synthesis is tied to growth.</p>
+                    <p class="center-txt ">Very often microbial cellular factories work beautifully under the optimal conditions in the lab, however, more often than not, these microbes never make it out of the lab and into production. This is because the conditions in the lab are usually ideal in terms of temperature, pressure, pH control and aeration rates. Yet, on the industrial scale, it can often be impossible to control these factors from batch to batch, or even spatially within a single batch due to the large size of industrial fermenters. BioTork’s evolutionary optimization technology is an effective tool for adapting cellular factories to the unique conditions associated with industrial fermentations. This technology allows microbes to perform at suboptimal temperatures and pH values as well as to tolerate more or less aeration as the situation requires. The result is a seamless and rapid scale up from the lab scale to the industrial scale.</p>
                     <p class="center-txt " > </p>
                 </div>
             </div>
@@ -28,4 +28,4 @@ function ProductYield() {
     );
 }
 
-export default ProductYield;
+export default Robust;
