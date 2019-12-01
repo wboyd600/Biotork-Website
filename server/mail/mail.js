0b8d26f8017c650216fb301a526dfad7b5418e52
Wesley Boyd <wboyd600@ufl.edu>
2019-12-01 17:50:17 -0500

    moved config info into config file, email works

--- server/mail/mail.js
+++ server/mail/mail.js
@@ -1,24 +1,39 @@
 const mailer = require("nodemailer");
-const {Hello} = require("./hello_template");
+const mg = require("nodemailer-mailgun-transport");
 const getEmailData = (firstName,lastName,email,subject,message) =>{
     let data = null;
             data = {
-                from: "Wesley Boyd <wboyd600@gmail.com>",
-                to: email,
-                subject: `Hello ${firstName}`,
-                html: Hello()
+                from: `${firstName} ${lastName} <${email}>`,
+                to: "wboyd600@ufl.edu",
+                subject: `${subject}`,
+                html: `<!DOCTYPE html>
+                <html style="margin: 0; padding: 0;">
+                
+                    <head>
+                        <title>Sent from Bio-Tork Contact Us</title>
+                    </head>
+                
+                        <body style="margin: 0; padding: 0;">
+                           <br />
+                           <br />
+                           <div>${message}</div>
+                           <br />
+                           <br />
+                        </body>
+                
+                  </html>
+                 `
             }
     return data;
 }
-
+const mailgunAuth = {
+    auth: {
+        api_key: require("../config/config").auth.api_key,
+        domain: require("../config/config").auth.domain
+      } 
+  } 
 const sendEmail = (firstName,lastName,email,subject,message) => {
-    const smtpTransport = mailer.createTransport({
-        service: "Gmail",
-        auth: {
-            user: "",
-            pass: ""
-        }
-    });
+    const smtpTransport = mailer.createTransport(mg(mailgunAuth));
     const mail = getEmailData(firstName,lastName,email,subject,message)
     smtpTransport.sendMail(mail,function(error,response){
         if(error){
